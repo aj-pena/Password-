715795ee1a974a481fc71c9004782efef6423a04
Andres Pena <penchjpc@gmail.com>
2021-09-20 21:58:58 -0400

    created set of conditionals to determine type of password to generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
+// Definition of variables to store user preferences regarding password criteria
 let userLength = 0;
+let uppercase = true;
+let lowercase = true;
+let numeric = true;
+let specialChar = true;
+let string = "";
 
-// Generate Password
+// Generate Password random characters
 function generatePassword(){
-  let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
   let result = "";
   for (i =0; i <= userLength; i++){
     let index = Math.floor(Math.random()*string.length - 1);
@@ -18,7 +23,7 @@ function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
+return;
 }
 
 // Add event listener to generate button
@@ -27,11 +32,63 @@ generateBtn.addEventListener("click", writePassword);
 // Prompt asking for a length. Verifies length is at least 8 and no more than 128 characters
 function passwordLength (){
   userLength = window.prompt("Please specify the number of characters in your password")
-  if(userLength < 8 || userLength > 128){
+  if(userLength < 8 || userLength > 128) {
     window.alert("The password must be at least 8 characters and no more than 128 characters")
     passwordLength();
-  } generatePassword();
+  } prompts();
 }
+// Prompts for user preferences regarding password criteria
+function prompts(){
+  uppercase = window.confirm("click OK if you would like your password to include uppercases, click cancel otherwise")
+  lowercase = window.confirm("click OK if you would like your password to include lowercases, click cancel otherwise")
+  numeric = window.confirm("click OK if you would like your password to include numbers, click cancel otherwise")
+  specialChar = window.confirm("click OK if you would like your password to include especial characters, click cancel otherwise")
+  if (uppercase != true && lowercase != true && numeric != true && specialChar != true){
+   window.alert("Please select at least one type of character")
+   prompts() 
+  }else if (uppercase && lowercase && numeric && specialChar){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+    generatePassword(0,userLength);
+  }else if (uppercase && lowercase !=true && numeric && specialChar){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$';
+    generatePassword();
+  }else if (uppercase != true && lowercase !=true && numeric && specialChar){
+    string = '0123456789@#$';
+    generatePassword();
+  }else if (uppercase !=true && lowercase !=true && numeric != true && specialChar){
+    string = '@#$';
+    generatePassword();
+  }else if (uppercase && lowercase && numeric && specialChar != true){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    generatePassword();
+  }else if (uppercase && lowercase && numeric !=true && specialChar !=true){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    generatePassword();
+  }else if (uppercase !=true && lowercase && numeric !=true && specialChar !=true){
+    string = 'abcdefghijklmnopqrstuvwxyz';
+    generatePassword();
+  }else if(uppercase && lowercase && numeric !=true && specialChar){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$';
+    generatePassword();
+  }else if (uppercase != true && lowercase && numeric !=true && specialChar){
+    string = 'abcdefghijklmnopqrstuvwxyz@#$';
+    generatePassword();
+  }else if (uppercase !=true && lowercase && numeric && specialChar){
+    string = 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+    generatePassword();
+  }else if (uppercase != true && lowercase !=true && numeric && specialChar !=true){
+    string = '0123456789';
+    generatePassword();
+  }else if (uppercase && lowercase !=true && numeric !=true && specialChar !=true){
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    generatePassword();
+  }else {
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    generatePassword();
+  }
+  return;
+}
+
 passwordLength();
 
 
